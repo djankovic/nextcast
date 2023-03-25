93b5660d4d3da986f0cec1a61b06d6ca71cf4032
Dom J <6401646+djankovic@users.noreply.github.com>
2023-03-25 15:23:20 +0100

    Enable xmerl

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Nextcast.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :inets, :eex],
+      extra_applications: [:logger, :inets, :eex, :xmerl],
       mod: {Nextcast.Application, []},
     ]
   end
