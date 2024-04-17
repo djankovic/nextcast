79df0e589bcef219658b0c8621ca880d6987a9fc
Dom <dominik@banoviez.com>
2024-04-17 20:06:19 +0200

    Release 0.20.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nextcast.MixProject do
   def project do
     [
       app: :nextcast,
-      version: "0.19.14",
+      version: "0.20.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
