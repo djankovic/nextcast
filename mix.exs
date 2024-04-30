3be7523b512d4644c5ed7407303130afadcd5085
Dom <dominik@banoviez.com>
2024-04-30 02:06:05 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nextcast.MixProject do
   def project do
     [
       app: :nextcast,
-      version: "0.21.1",
+      version: "0.22.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
